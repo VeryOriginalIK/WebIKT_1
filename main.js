aa700cb0b5e57bd34c4ee6e3e1c73446269c84fb
Vörös Bence <100135396+VeryOriginalIK@users.noreply.github.com>
2023-10-25 00:32:27 +0200

    ez van

--- main.js
+++ main.js
@@ -3,21 +3,15 @@ function kemeny() {
     var imgBoxes = document.getElementsByClassName("imgBox");
     if (imgBoxes.length > 0) {
         imgBoxes[0].style.transform = "rotateY(-180deg)";
-    } else {
-        console.error("No elements with class 'imgBox' found.");
     }
-    
-    
     var imgBoxElement = document.querySelector(".imgBox");
 
     if (imgBoxElement) {
         var childElements = imgBoxElement.children;
-
         for (var i = 0; i < childElements.length; i++) {
             childElements[i].style.opacity = "0";
         }
     }
-
     var cardElement = document.querySelector(".card");
     if (cardElement) {
         cardElement.style.boxShadow = "inset 20vh 0 3vh rgba(0, 0, 0, 0.5), 2vh 0 6vh rgba(0, 0, 0, 0.5)";
@@ -25,7 +19,7 @@ function kemeny() {
     }
     document.querySelector("#kemeny").style.display = "block";
     document.querySelector("#ada").style.display = "none";
-    document.querySelector("#keszitok").style.display = "none";
+    document.querySelector("#basic").style.display = "none";
 }
 
 function ada() {
@@ -34,21 +28,15 @@ function ada() {
     console.log(imgBoxes)
     if (imgBoxes.length > 0) {
         imgBoxes[0].style.transform = "rotateY(-180deg)";
-    } else {
-        console.error("No elements with class 'imgBox' found.");
     }
     
-    
     var imgBoxElement = document.querySelector(".imgBox");
-
     if (imgBoxElement) {
         var childElements = imgBoxElement.children;
-
         for (var i = 0; i < childElements.length; i++) {
             childElements[i].style.opacity = "0";
         }
     }
-
     var cardElement = document.querySelector(".card");
     if (cardElement) {
         cardElement.style.boxShadow = "inset 20vh 0 3vh rgba(0, 0, 0, 0.5), 2vh 0 6vh rgba(0, 0, 0, 0.5)";
@@ -56,16 +44,13 @@ function ada() {
     }
     document.querySelector("#kemeny").style.display = "none";
     document.querySelector("#ada").style.display = "block";
-    document.querySelector("#keszitok").style.display = "none";
+    document.querySelector("#basic").style.display = "none";
 }
 
-function keszitok() {
+function basic() {
     var imgBoxes = document.getElementsByClassName("imgBox");
     if (imgBoxes.length > 0) {
         imgBoxes[0].style.transform = "rotateY(-180deg)";
-    } else {
-        console.error("No elements with class 'imgBox' found.");
-    }
     
     
     var imgBoxElement = document.querySelector(".imgBox");
@@ -85,6 +70,5 @@ function keszitok() {
     }
     document.querySelector("#kemeny").style.display = "none";
     document.querySelector("#ada").style.display = "none";
-    document.querySelector("#keszitok").style.display = "block";
-}
-
+    document.querySelector("#basic").style.display = "block";
+}}
